7ab5db777ff32853ba63fa7f1be150d0c03fdbb0
Ahmed Adel Rashwan <153387067+AhmedAdelRashwan@users.noreply.github.com>
2024-01-24 13:43:18 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+function show(){
+    let pargOne= document.getElementById("pargOne")
+    let buOne= document.getElementById("buttonOne")
+    let butwo =document.getElementById("buttonTwo")
+    if(pargOne.style.display="none"){pargOne.style.display="block",
+    butwo.style.display="block",buOne.style.display="none"}
+}
+function hide(){
+    let pargOne= document.getElementById("pargOne")
+    let buOne= document.getElementById("buttonOne")
+    let butwo =document.getElementById("buttonTwo")
+    if(pargOne.style.display="block"){pargOne.style.display="none",butwo.style.display="none",
+    buOne.style.display="block"}
+} 
+function showTwo (){
+    let pargTwo= document.getElementById("pargTwo");
+    let buThree= document.getElementById("buttonThree");
+    let buFour= document.getElementById("buttonFour");
+    if(pargTwo.style.display="none"){pargTwo.style.display="block",
+    buFour.style.display="block",buThree.style.display="none"}
+}
+function hideTwo(){
+    let pargTwo= document.getElementById("pargTwo")
+    let buThree= document.getElementById("buttonThree")
+    let buFour =document.getElementById("buttonFour")
+    if(pargTwo.style.display="block"){pargTwo.style.display="none",buFour.style.display="none",
+    buThree.style.display="block"}
+} 
+function showThree (){
+    let pargThree= document.getElementById("pargThree");
+    let buFive= document.getElementById("buttonFive");
+    let buSix= document.getElementById("buttonSix");
+    if(pargThree.style.display="none"){pargThree.style.display="block",
+    buSix.style.display="block",buFive.style.display="none"}
+}
+function hideFour(){
+    let pargThree= document.getElementById("pargThree")
+    let buFive= document.getElementById("buttonFive")
+    let buSix =document.getElementById("buttonSix")
+    if(pargThree.style.display="block"){pargThree.style.display="none",buSix.style.display="none",
+    buFive.style.display="block"}
+} 
+function showFive (){
+    let pargFour= document.getElementById("pargFour");
+    let buSeven= document.getElementById("buttonSeven");
+    let buEight= document.getElementById("buttonEight");
+    if(pargFour.style.display="none"){pargFour.style.display="block",
+    buEight.style.display="block",buSeven.style.display="none"}
+}
+function hideFive(){
+    let pargFour= document.getElementById("pargFour")
+    let buSeven= document.getElementById("buttonSeven")
+    let buEight =document.getElementById("buttonEight")
+    if(pargFour.style.display="block"){pargFour.style.display="none",buEight.style.display="none",
+    buSeven.style.display="block"}
+} 
+function showSix (){
+    let pargFive= document.getElementById("pargFive");
+    let buNine= document.getElementById("buttonNine");
+    let buTen= document.getElementById("buttonTen");
+    if(pargFive.style.display="none"){pargFive.style.display="block",
+    buTen.style.display="block",buNine.style.display="none"}
+}
+function hideSix(){
+    let pargFive= document.getElementById("pargFive")
+    let buNine= document.getElementById("buttonNine")
+    let buTen =document.getElementById("buttonTen")
+    if(pargFive.style.display="block"){pargFive.style.display="none",buTen.style.display="none",
+    buNine.style.display="block"}
+} 
